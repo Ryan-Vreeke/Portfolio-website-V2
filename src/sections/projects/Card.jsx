f1fdeaeb646647c4d30a64169e2b7fe1e454d9ee
Ryan-Vreeke <rvreeke1234@gmail.com>
2023-06-11 17:19:18 -0600

    completed project card seciton

--- src/sections/projects/Card.jsx
+++ src/sections/projects/Card.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import './projects.css'
 
 function Card(props) {
-  const {name, des, image} = props;
+  const {name, des, image, type, src} = props;
 
   return (
-    <div className="project_card">
+    <a src={src} className='project_card'>
         <img src={image} alt="" />
-        <h3 className="project_name">{name}</h3>
-        <h4 className="project_des">{des}</h4>
-    </div>
+        <div className="project_text">
+          <h1>{name}</h1>
+          <h2>{type}</h2>
+
+          <p>{des}</p>
+        </div>
+    </a>
   )
 }
 
--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -4,15 +4,15 @@ import './projects.css'
 import image from '../../portfolio.jpg'
 
 function Projects() {
-  const count = 4;
+  const count = 5;
   return (
     <section id='Projects'>
       <h3 className='project_title'>Projects</h3>
-      <div className="card_section">
-        {Array.from({length: count }, (_, index) => (
-          <Card name={`Project ${index}`} des='this is a test' image={image} />
+      <div className="project_scroll">
+
+        {Array.from({ length: count }, (_, index) => (
+          <Card name={`Project ${index}`} des='this is a test' image={image} type='C#'/>
         ))}
-    
       </div>
     </section>
   )
